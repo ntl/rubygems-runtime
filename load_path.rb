44f960951aecebd5d91503c8a4eed00542adf6f0
Nathan Ladd <nathanladd@gmail.com>
2025-08-14 16:07:15 -0400

    load_path.rb is updated

--- load_path.rb
+++ load_path.rb
@@ -1,13 +1,15 @@
+libraries_dir = ENV['LIBRARIES_HOME']
+if not libraries_dir.nil?
+  symlinked_bundler_setup_rb_pattern = File.expand_path('*-bundler-setup.rb', libraries_dir)
+
+  Dir.glob(symlinked_bundler_setup_rb_pattern) do |symlinked_bundler_setup_rb|
+    require_relative(symlinked_bundler_setup_rb)
+  end
+end
+
 gems_lib_dir = File.expand_path('gems/lib', __dir__)
 if not $LOAD_PATH.include?(gems_lib_dir)
   $LOAD_PATH.unshift(gems_lib_dir)
 end
 
-libraries_dir = ENV['LIBRARIES_HOME']
-if not libraries_dir.nil?
-  symlinked_bundle_setup_rb_pattern = File.expand_path('*-bundle-setup.rb', libraries_dir)
-
-  Dir.glob(symlinked_bundle_setup_rb_pattern) do |symlinked_bundle_setup_rb|
-    require_relative(symlinked_bundle_setup_rb)
-  end
-end
+require 'bundler/setup'
